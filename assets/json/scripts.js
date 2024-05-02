526a416b92af69879fbb1d0bf2d891683fa04595
primetimetran <primetimetran@gmail.com>
2024-05-02 10:15:18 -0400

    Fix initial test case values

--- assets/json/scripts.js
+++ assets/json/scripts.js
@@ -14,6 +14,15 @@ function writeJsonFile(filePath, data) {
   fs.writeFileSync(filePath, jsonData, 'utf8')
 }
 
+function getParameterType(value) {
+  if (typeof value === 'string') return 'string'
+  if (typeof value === 'number')
+    return Number.isInteger(value) ? 'int' : 'double'
+  if (typeof value === 'boolean') return 'bool' // Check for boolean type
+  if (Array.isArray(value)) return 'list'
+  return 'unknown'
+}
+
 function updateTestSuiteObjects() {
   let data = readJsonFile(filePath)
   data = data.data.map((question) => {
@@ -34,38 +43,46 @@ function updateTestSuiteObjects() {
 function addId() {
   let data = readJsonFile(filePath)
   data = data.data.map((question, idx) => {
-    question.id = idx+1;
+    question.id = idx + 1
     return question
   })
   writeJsonFile(filePath, data)
 }
 
 function addSignature() {
-  function getParameterType(value) {
-    if (typeof value === 'string') return 'string';
-    if (typeof value === 'number') return Number.isInteger(value) ? 'int' : 'double';
-    if (typeof value === 'boolean') return 'bool'; // Check for boolean type
-    if (Array.isArray(value)) return 'list';
-    return 'unknown';
-  }
-
-  let data = readJsonFile(filePath);
+  let data = readJsonFile(filePath)
   data = data.data.map((question, idx) => {
     if (question.testSuite && question.testSuite[0].inputs) {
-      const parameters = Object.entries(question.testSuite[0].inputs).map(([key, value], idx) => ({
-        type: getParameterType(value.value),
-        name: question.signature.parameters[idx].name
-      }));
-  
+      const parameters = Object.entries(question.testSuite[0].inputs).map(
+        ([key, value], idx) => ({
+          type: getParameterType(value.value),
+          name: question.signature.parameters[idx].name,
+        })
+      )
+
       question.signature = {
         parameters,
-        returnType: getParameterType(question.testSuite[0].output)
-      };
+        returnType: getParameterType(question.testSuite[0].output),
+      }
     }
-    return question;
-  });
-  writeJsonFile(filePath, { data });
+    return question
+  })
+  writeJsonFile(filePath, { data })
 }
 
+// addSignature()
 
-addSignature()
+function changeInputType() {
+  let data = readJsonFile(filePath)
+  data = data.data.map((question) => {
+    if (question.testSuite) {
+      question.testSuite.forEach((testCase, idx) => {
+        const inputs = testCase.inputs.map((input) => input.value)
+        question.testSuite[idx].input = inputs
+      })
+    }
+    return question
+  })
+  writeJsonFile(filePath, { data });
+}
+changeInputType()
